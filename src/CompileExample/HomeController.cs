97a58726652e7db556d4ca1c41c490a6cf7d3364
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2015-09-24 17:31:20 +0200

    Add project

---
+++ src/CompileExample/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace CompileExample
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
